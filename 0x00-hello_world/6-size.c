b677126b98cb8b3566a6db162a19abb88af1460e
Juweria86 <jujumelmi143@gmail.com>
2023-07-13 13:13:41 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
- * main -Aprogram the size of various types on the computer
- * Return: 0
+/**
+ * main -Aprogram that prints the size of various types on the computer
+ * Return: 0(pass)
  */
 int main(void)
 {
