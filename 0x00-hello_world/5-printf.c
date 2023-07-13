6d400f951a163eb2f793ed8f8a0468740983c20f
Juweria86 <jujumelmi143@gmail.com>
2023-07-13 11:01:15 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Aprogram that outputs line using printf function
+ * returns:0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
