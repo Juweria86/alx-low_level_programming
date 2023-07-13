0a1ed090c1695a4a99a7a9673346d880bdbf72fd
Juweria86 <jujumelmi143@gmail.com>
2023-07-13 13:50:43 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unisted>
+/**
+ * main -Aptogram that prints line to standard e
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
