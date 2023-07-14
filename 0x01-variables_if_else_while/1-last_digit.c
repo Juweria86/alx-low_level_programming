77ec8b8137ce4983e3e099e14ca812f61f4989de
Juweria86 <jujumelmi143@gmail.com>
2023-07-14 13:22:33 -0700

    assigns last digit to variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,6 @@ int main(void)
 	if (lastDigit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
-		
 	}
 	else if (lastDigit == 0)
 	{
@@ -25,7 +24,6 @@ int main(void)
 	else if (lastDigit < 6 || lastDigit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
-	
 	}
 	return (0);
 }
