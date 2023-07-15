b2bfd496501fe628236b052867bb1b0ca399d6b6
Juweria86 <jujumelmi143@gmail.com>
2023-07-15 08:02:01 -0700

    combination of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Aprogram that prints all possible combination of single digits
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + 48);
+		if (i == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
