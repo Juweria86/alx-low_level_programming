dff247aba86f3dd5d7ca8aef6e51ea593265a4e9
Juweria86 <jujumelmi143@gmail.com>
2023-07-15 05:59:24 -0700

    prints lower and upper case alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,18 +6,18 @@
 int main(void)
 {
 	int n = 97;
-	int m =65;
+	int m = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
 		n++;
 	}
-	while (m <= 122)
+	while (m <= 90)
 	{
 		putchar(m);
 		n++;
 	}
 	putchar('\n');
-	return (0);
+	return (0)
 }
