bdbcdf7582c54a085878769c299dffa22747b1c0
Juweria86 <jujumelmi143@gmail.com>
2023-07-18 14:38:41 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,21 +6,17 @@
  */
 void print_to_98(int n)
 {
-	int count;
-
-	if (n > 98)
+	while (n != 98)
 	{
-		for (count = n; count > 98; count--)
-		{
-			printf("%d, ", count);
-		}
+		printf("%d, ", n);
+		if (n > 98)
+			--n;
+		else
+			++n;
 	}
-	else
+	if (n == 98)
 	{
-		for (count = n; count < 98; count++)
-		{
-			printf("%d, ", count);
-		}
+		printf("%d\n", n);
 	}
 }
 
