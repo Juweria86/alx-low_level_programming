ee9c2170c2f17ce08429a75ecca08fcd3d38deb6
Juweria86 <jujumelmi143@gmail.com>
2023-07-18 15:34:42 -0700

    50 fibonnacci numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main -a program that computes and prints the sum of all the multiples of 3 or 5.
+ *Return: 0
+ */
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if ((i % 5 == 0) || (i % 3 == 0))
+		{
+			sum = sum + i;
+		}
+		printf("%d\n",sum);
+		return (0);
+	}
+}
+
