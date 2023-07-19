0edf92f6c4d5f0d21c2e0add7eb0e54bbb951c3f
Juweria86 <jujumelmi143@gmail.com>
2023-07-19 01:27:44 -0700

    sum of natural numbers belo 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,14 @@ int main(void)
 	int sum = 0;
 	int i;
 
-	for (i = 0; i <= 1024; i++)
+	for (i = 1; i <= 1024; i++)
 	{
 		if ((i % 5 == 0) || (i % 3 == 0))
 		{
-			sum = sum + i;
+			sum += i;
 		}
-		printf("%d\n",sum);
-		return (0);
 	}
+	printf("%d\n", sum);
+	return (0);
 }
 
