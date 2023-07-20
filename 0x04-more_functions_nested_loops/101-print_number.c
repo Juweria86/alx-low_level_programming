60d9dcdf7c2af71b44cc937014b21b52b94e1445
juweria mohamood <juju143@gmail.com>
2023-07-21 01:34:40 +0300

    prints numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,15 +5,18 @@
  */
 void print_number(int n)
 {
-	unsigned int i = n;
+	int i;
 
-	if (n > 0)
+	i = n;
+
+	if (n < 0)
 	{
-		_putchar(45);
+		_putchar('-');
 		i = -i;
 	}
 	if (i / 10)
+	{
 		print_number(i / 10);
+	}
 	_putchar(i % 10 + '0');
 }
-
