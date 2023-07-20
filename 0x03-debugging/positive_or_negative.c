25b40f1548478a6004f89f205be76d8c0a7b676a
juweria mohamood <juju143@gmail.com>
2023-07-20 18:44:38 +0300

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -Aprogram that checks if n is positive or negative
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
