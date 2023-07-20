046fe9ebd1a7a10b48aa1850ca4e0f0ec522e521
juweria mohamood <juju143@gmail.com>
2023-07-20 22:39:10 +0300

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle -prints triangle
+ *@size: an integer
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j, k;
+
+		for (i = 0; i < size; i++)
+		{
+			for (k = size - i; k > 0; k--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
