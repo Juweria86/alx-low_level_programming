dbb4780515a014f98204ed3d80c20036f38358ef
juweria mohamood <juju143@gmail.com>
2023-07-24 19:35:42 +0300

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen -function that returns the length of a string.
+ *@s: character
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
