44fc0dc13eed3f90024de04bfe6ed8ad67bea638
juweria mohamood <juju143@gmail.com>
2023-07-25 16:28:45 +0300

    prints string in half

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,23 @@
 #include "main.h"
+/**
+ * rev_string -prints string in reverse.
+ */
+void rev_string(char *s)
+{
+	int len = _strlen(s);
+	char temp;
+	int i;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+	for (i = 0; i < len; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
 
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half -function that prints half of a string
+ * @str: pointer
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int start;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	start = len / 2;
+	if (len % 2 != 0)
+	{
+		start = (len - 1) / 2;
+	}
+	for (i = start; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
