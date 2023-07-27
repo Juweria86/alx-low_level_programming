c349c58491f9a255fe81fc5ef8a454d4e76d58d9
juweria mohamood <juju143@gmail.com>
2023-07-27 21:14:46 +0300

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,18 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 != *s2)
+		if (s1[i] != s2[i])
 		{
-			return ((*s1 < *s2) ? - 15 : 15);
+			return (s1[i] - s2[i]);
 		}
-		s1++;
-		s2++;
+		i++;
 	}
-	if (*s1 == '\0' && *s2 == '\0')
-	{
-		return (0);
-	}
-	return ((*s1 < *s2) ? -15 : 15);
+	return (0);
 }
