a7ffe7f3bc3d34d9425c508d32f3f0095f44713e
juweria mohamood <juju143@gmail.com>
2023-07-27 13:21:41 +0300

    rev array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,14 +4,14 @@
  * @a: pointer
  * @n: integer
  */
-void reverse_array(int *a, int n) 
+void reverse_array(int *a, int n)
 {
 	int i, j, temp;
-	for (i = 0, j = n - 1; i < j; i++, j--) 
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
 	{
 		temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 	}
-	_putchar('\n');
 }
