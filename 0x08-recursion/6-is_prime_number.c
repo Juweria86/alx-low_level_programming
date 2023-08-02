4387aeacc38dc4ccf9f1398ad07a6c47e2056b4e
juweria mohamood <juju143@gmail.com>
2023-08-02 23:49:58 +0300

    solving errorsgit add .git add .!

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,22 +3,34 @@
  * is_prime_number -function that returns 1 if the input integer is
  * a prime number, otherwise return 0
  * @n: an integer
- * Return: 1
+ * @i: integer
+ * Return: is_prime_helper
  */
+int is_prime_helper(int n, int i);
+
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-	for (i = 2; i <= n / 2; i++)
+	return (is_prime_helper(n, 2));
+}
+/**
+ * is_prime_helper -checks for prime number
+ * @n: integer
+ * @i: integer
+ * Return: is__prime_helper
+ */
+int is_prime_helper(int n, int i)
+{
+	if (i > n / 2)
 	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
+		return (1);
 	}
-	return (1);
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_helper(n, i + 1));
 }
