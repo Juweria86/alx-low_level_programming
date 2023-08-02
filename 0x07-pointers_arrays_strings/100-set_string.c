deda24e18a1a1da37b3f6e65afdd9546126ca973
Juweria86 <jujumelmi143@gmail.com>
2023-08-02 05:38:32 +0300

    string to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/** set_string -set the value of apointer to a char
+ * @s: pointer  to pointer
+ * @to: pointer to char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
