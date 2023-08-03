46420087b1e5eea3e846016acb231c57cc3e3a40
Juweria86 <jujumelmi143@gmail.com>
2023-08-03 20:40:59 +0300

    fixing some betty errors

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * _strlen_recursion -prints length of length
- *   * @s: pointer
- *    * Return: length of string
- *     */
+ * _strlen_recursion -prints length of length
+ * @s: pointer
+ * Return: length of string
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
@@ -23,12 +23,12 @@ int comparator(char *s, int n1, int n2)
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
-		return(0 + comparator(s, n1 + 1, n2 - 1));
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
 /**
- * is_palindrome -function that returns 1 
+ * is_palindrome -function that returns 1
  * if a string is a palindrome and 0 if not.
  * @s: string
  * Return: 1 if palindrome else return 0.
@@ -37,5 +37,5 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return comparator(s, 0, _strlen_recursion(s) - 1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
