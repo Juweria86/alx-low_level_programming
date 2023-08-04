5dfe02012508aa3d36da67f42ee45a165ad722b9
Juweria86 <jujumelmi143@gmail.com>
2023-08-04 16:05:49 +0300

    creating static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs -prints absolute value of integer.
+ * @i: an integer.
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+		if (i >= 0)
+		{
+			return (i);
+		}
+		else
+		{
+			return (i * -1);
+		}
+}
