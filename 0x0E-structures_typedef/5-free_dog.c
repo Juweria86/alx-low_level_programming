3470117fb3eaa1f71de85c63d8816f8ee1969d8a
Juweria86 <jujumelmi143@gmail.com>
2023-08-14 18:24:39 +0300

    freeing dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 /**
  * free_dog -  function that frees dogs.
- *@dog:pointer
+ * @dog:pointer
  */
 
-void free_dog(dog_t* dog)
+void free_dog(dog_t *dog)
 {
 	if (dog == NULL)
-		return; 
-	
+		return;
 	free(dog->name);
 	free(dog->owner);
 	free(dog);
