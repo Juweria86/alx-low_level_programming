4db68efb613e25e4760b611489d8b3edd6903ad4
Juweria86 <jujumelmi143@gmail.com>
2023-08-28 14:09:18 +0300

    solving task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h->next != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
-	printf("%d\n", h->n);
-	count++;
 	return (count);
 }
