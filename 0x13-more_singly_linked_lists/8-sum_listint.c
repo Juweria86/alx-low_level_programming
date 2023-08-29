cb5276e02f712cddf0a17d729584d2d9191f9958
Juweria86 <jujumelmi143@gmail.com>
2023-08-29 22:51:09 +0300

    task8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all the data
+ * @head: pointer
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->data;
+		current = current->next;
+	}
+	return (sum);
+}
