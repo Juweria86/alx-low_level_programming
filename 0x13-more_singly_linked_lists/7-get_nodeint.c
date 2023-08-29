e21187ab6816c602e45666653edc751aa4de54cd
Juweria86 <jujumelmi143@gmail.com>
2023-08-29 22:44:50 +0300

    task7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,17 +7,18 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *new_node, *temp;
-	unsigned int i;
+	listint_t *current;
+	unsigned int i = 0;
 
-	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
-		return (NULL);
-
-	temp = head;
-	for (i = 0; i < index; i++)
-		temp = temp->next;
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
+	current = head;
+	while (current != NULL)
+	{
+		if (i == index)
+		{
+			return (current);
+		}
+		i++;
+		current = current->next;
+	}
+	return (NULL);
 }
