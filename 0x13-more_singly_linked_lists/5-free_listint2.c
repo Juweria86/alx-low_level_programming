90ecd547ed70d4b78d57af1e87241a3f44e9db59
Juweria86 <jujumelmi143@gmail.com>
2023-08-29 11:56:48 +0300

    freeing list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees list and sets head to null.
+ * @head: pointer to first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+		*head = NULL;
+	}
+}
