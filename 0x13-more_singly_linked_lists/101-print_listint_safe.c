f82355ee17d6406b27bf50c9cf43281eb6e55458
Juweria86 <jujumelmi143@gmail.com>
2023-08-31 05:32:55 +0300

    making some changes

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,46 +1,5 @@
 #include "lists.h"
-
 size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
-/**
- * looped_listint_len - Counts the number of unique nodes
- * in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
- */
-size_t looped_listint_len(const listint_t *head)
-{
-	const listint_t *tortoise, *hare;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-	tortoise = head->next;
-	hare = (head->next)->next;
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
-			return (nodes);
-		}
-		tortoise = tortoise->next;
-		hare = (hare->next)->n;
-			return (0);
-}
 /**
  * print_listint_safe - Prints a listint_t list safely.
  * @head: A pointer to the head of the listint_t list.
@@ -69,4 +28,45 @@ size_t print_listint_safe(const listint_t *head)
 		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 	return (nodes);
+}
+
+/**
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *sl, *fa;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	sl = head->next;
+	fa = (head->next)->next;
+	while (fa)
+	{
+		if (sl == fa)
+		{
+			sl = head;
+			while (sl != fa)
+			{
+				nodes++;
+				sl = sl->next;
+				fa = fa->next;
+			}
+			sl = sl->next;
+			while (sl != fa)
+			{
+				nodes++;
+				sl = sl->next;
+			}
+			return (nodes);
+		}
+		sl = sl->next;
+		fa = (fa->next)->next;
+	}
+	return (0);
 }
