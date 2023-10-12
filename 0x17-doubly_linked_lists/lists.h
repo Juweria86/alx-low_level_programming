52d7a97c4c9c003c022e8299baf45dbe461052c3
Juweria86 <jujumelmi143@gmail.com>
2023-10-12 22:22:44 +0300

    updating header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -16,6 +17,7 @@ typedef struct dlistint_s
     int n;
     struct dlistint_s *prev;
     struct dlistint_s *next;
+    struct dlistint_s *tail;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
