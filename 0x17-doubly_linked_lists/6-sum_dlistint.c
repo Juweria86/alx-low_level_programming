204ef3e1ede2c359e134fc214655eaa6c3b4a53c
Juweria86 <jujumelmi143@gmail.com>
2023-10-12 23:05:38 +0300

    prints sum of list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_dlistint - prints sum of linked list
+ * @head: pointer to first node
+ * Return: sum of list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
