959288e777977b22968f152cb311f69a5eb1a5f6
Juweria86 <jujumelmi143@gmail.com>
2023-10-13 05:54:03 +0300

    header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -17,7 +17,6 @@ typedef struct dlistint_s
     int n;
     struct dlistint_s *prev;
     struct dlistint_s *next;
-    struct dlistint_s *tail;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
